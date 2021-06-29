b34f8342c26b33d7ab286d97f96d5d3554fa6f06
Axel <axelvieira94@gmail.com>
2021-06-29 15:47:31 -0300

    validação

--- projeto-am-v2/configuracoes/conf.py
+++ projeto-am-v2/configuracoes/conf.py
@@ -1,3 +1,3 @@
-BASE_DE_DADOS = '/Volumes/GoogleDrive/Meu Drive/Doutorado/Disciplinas/Aprendizagem de Máquina/projeto/codigo/ml-project/base_de_dados/yeast1.dat'
+BASE_DE_DADOS = 'C:/Users/kalinx/Documents/Python Scripts/Projeto-AM/projeto-am-v2/base_de_dados/yeast1.dat'
 ROTULOS_ATRIBUTOS = ["LINHA", "NUC", "VAC", "POX", "ERL", "MIT", "ALM", "GVH", "MCG", "SEQUENCE NAME"]
 ROTULOS_CLASSES = ["CYT", "NUC", "MIT", "ME3", "ME2", "ME1", "EXC", "VAC", "POX", "ERL"]
